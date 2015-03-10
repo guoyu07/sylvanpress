24066ed746cd13701f88916510f80daa47137ce2
James Acklin <james@acklindesign.com>
2015-03-10 09:19:42 -0400

    Some more custom fields for homepage

--- content-home.php
+++ content-home.php
@@ -9,15 +9,15 @@
   <div class="hero-home">
     <div class="content">
       <h1><img src="<?=get_template_directory_uri()?>/images/logo-white-large.png" alt="Sylvan Gardens Landscape Contractors"></h1>
-      <p class="intro"><?php the_field('company_description')?></p>
-      <a href="#" class="button button-primary">See Our Work</a>
+      <p class="intro"><?php the_field('company_description'); ?></p>
+      <a href="<?php the_field('link_to_portfolio'); ?>" class="button button-primary"><?php the_field('portfolio_link_text'); ?></a>
     </div>
   </div>
 
   <div class="home-content">
     <div class="mission">
       <img src="<?=get_template_directory_uri()?>/images/plant-drawing.png" alt="">
-      <p><span class="large">Our mission</span> is to provide strong customer service at a fair price. We work cooperatively with our customers to design, install and manage the highest-quality landscapes. We are devoted to the principles of small business and committed to ensuring that all customers and employees are treated respectfully and equitably.</p>
+      <p><?php the_field('company_mission'); ?></p>
     </div>
     <div class="callouts">
       <div class="callout">
