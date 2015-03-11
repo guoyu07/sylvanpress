10eb0d4e0d825ce8fb543e28c0028d9aebfbfdb5
James Acklin <james@acklindesign.com>
2015-03-10 22:27:51 -0400

    Sidebar section repeater

--- content-page.php
+++ content-page.php
@@ -7,20 +7,33 @@
 ?>
 
 
-
 <div class="hero-page">
-	<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-    <p class="subhead">Some secondary text if necessary. Lorem ipsum dolor sit amet.</p>
+  <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+  <p class="subhead">Some secondary text if necessary. Lorem ipsum dolor sit amet.</p>
 </div>
 
 <div class="page">
-	<div class="page-content">
-  		<div class="breadcrumbs">
-  			<ul class="breadcrumb-trail"></ul>
-  		</div>
-  		<article id="post-<?php the_ID(); ?>">
-  			<?php the_content(); ?>
-  		</article>
-
-  	</div>
+  <div class="page-content">
+    <div class="breadcrumbs">
+      <ul class="breadcrumb-trail"></ul>
+    </div>
+    <article id="post-<?php the_ID(); ?>">
+      <?php the_content(); ?>
+    </article>
+    <?php if(have_rows('sidebar_blocks')): ?>
+      <aside>
+        <?php while( have_rows('sidebar_blocks') ): the_row(); ?>
+          <section>
+            <h4><?php the_sub_field('section_heading') ?></h4>
+            <?php the_sub_field('section_content') ?>
+            <?php if(get_sub_field('section_button_link')): ?>
+              <a class="button button-normal" href="<?php the_sub_field('section_button_link') ?>">
+                <?php the_sub_field('section_button_text'); ?>
+              </a>
+            <?php endif; ?>
+          </section>
+        <?php endwhile; ?>
+      </aside>
+    <?php endif; ?>
+  </div>
 </div>
